4f806780406da85fc0843ebca042bf8df515da5b
Lellansin <lellansin@gmail.com>
2015-05-20 14:21:39 +0800

    Update caesar.py

--- python/Caesar.py
+++ python/Caesar.py
@@ -5,32 +5,32 @@
 # @author  lellansin <lellansin@gmail.com>
 # @website http://www.lellansin.com/tutorials/ciphers
 #
-import re
 
-A = ord('A')
-Z = ord('Z')
-
-def getAsc(asc):
-    if asc > Z:
-        return (asc - Z) % 26 - 1 + A
-    elif asc < A:
-        return Z - ((A - asc) % 26 - 1)
+def getShiftCh(ch, shift):
+    if str.isalpha(str(ch)):
+        asc = ord(ch) + shift;
+        new_ch = asc
+        if asc > ord('Z'):
+            new_ch = (asc - ord('Z')) % 26 - 1 + ord('A')
+        elif asc < ord('A'):
+            new_ch = ord('Z') - ((ord('A') - asc) % 26 - 1)
+        return unichr(new_ch)
     else:
-        return asc
+        return ch
 
+# 
+# 凯撒密码
+# 
 def caesar(words, shift):
     cipher = ''
-    for ch in re.compile(r'\W').sub('', words.upper()):
-        cipher += str(unichr(getAsc(ord(ch) + shift)))
+    for ch in words.upper():
+        cipher += getShiftCh(ch, shift)
     return cipher.lower()
 
 if __name__ == '__main__':
     text = 'hello world, this is Caesar cipher.'
-    # text = 'abcdefghijklmnopqrstuvwxyz'
-
     # 右移
     ciphertext = caesar(text, 3);
     print(ciphertext)
-
     # 左移
     print(caesar(ciphertext, -3))
