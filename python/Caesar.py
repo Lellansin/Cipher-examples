023f6e41200a39b9b2419899c2578c4e316be36d
Lellansin <lellansin@gmail.com>
2015-05-20 12:58:20 +0800

    添加凯撒密码

---
+++ python/Caesar.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Caesar cipher
+# 
+# @author  lellansin <lellansin@gmail.com>
+# @website http://www.lellansin.com/tutorials/ciphers
+#
+import re
+
+A = ord('A')
+Z = ord('Z')
+
+def getAsc(asc):
+    if asc > Z:
+        return (asc - Z) / 26 + A
+    elif asc < A:
+        return (A - asc) / 26 + A
+    else:
+        return asc
+
+def caesar(words, shift):
+    cipher = ''
+    for ch in re.compile(r'\W').sub('', words.upper()):
+        cipher += str(unichr(getAsc(ord(ch) + shift)))
+    return cipher.lower()
+
+if __name__ == '__main__':
+    text = 'hello world, this is Caesar cipher.'
+
+    # 右移
+    ciphertext = caesar(text, 3);
+    print(ciphertext)
+
+    # 左移
+    print(caesar(ciphertext, -3))
