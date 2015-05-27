247150dc026884b294b06b2e916e8623687267f2
Lellansin <lellansin@gmail.com>
2015-05-27 21:14:16 +0800

    添加 playfair.py

---
+++ python/Playfair.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Playfair cipher
+# 
+# @author  lellansin <lellansin@gmail.com>
+# @website http://www.lellansin.com/tutorials/ciphers
+#
+
+
+
+if __name__ == '__main__':
+    pass
