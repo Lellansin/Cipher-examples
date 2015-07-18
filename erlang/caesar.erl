c50187afaa69115ec87d9e4c1a2f6ae69014205b
Lellansin <lellansin@gmail.com>
2015-07-18 22:01:06 +0800

    更新 caesar.erl

--- erlang/caesar.erl
+++ erlang/caesar.erl
@@ -15,28 +15,25 @@ slashes([]) -> [];
 slashes([ H | _ ]) when H =:= "~~" -> "\\";
 slashes([ H | T ]) -> [ H | slashes(T) ].
 
-shift(C, Shift) when ((C >= $A) and (C =< $Z)) or
-                     ((C >= $a) and (C =< $z)) -> 
+shift(C, Shift) when ((C >= $A) and (C =< $Z)) ->
     Offset = Shift rem 26,
-    case (C >= $a) of
-    	true -> 
-    		if
-                Offset + C > $z  ->
-                     $a + (Offset + C) - $z;
-                Offset + C < $a  ->
-                     $z + Offset + C;
-                true ->
-                     C + Offset
-            end;
-        false -> 
-            if
-                Offset + C > $Z  ->
-                     $A + (Offset + C) - $Z;
-                Offset + C < $A  ->
-                     $Z + Offset + C;
-                true ->
-                     C + Offset
-            end
+    if
+        Offset + C > $Z  ->
+             $A + (Offset + C) - $Z;
+        Offset + C < $A  ->
+             $Z + Offset + C;
+        true ->
+             C + Offset
+    end;
+shift(C, Shift) when ((C >= $a) and (C =< $z)) ->
+    Offset = Shift rem 26,
+	if
+        Offset + C > $z  ->
+             $a + (Offset + C) - $z;
+        Offset + C < $a  ->
+             $z + Offset + C;
+        true ->
+             C + Offset
     end;
 shift(C, _) -> C.
 
