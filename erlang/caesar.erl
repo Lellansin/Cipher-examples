349a61f5ecc175425b746f15a13c941367e8a28f
Lellansin <lellansin@gmail.com>
2015-07-19 17:32:24 +0800

    更新 caesar.erl

--- erlang/caesar.erl
+++ erlang/caesar.erl
@@ -7,38 +7,46 @@
 -module(caesar).
 -export([encrypt/2, decrypt/2, test/0]).
 
+% 
+% 加密
+% 
 encrypt(Words, Shift) -> [ shift(C, Shift) || C <- slashes(Words) ].
+
+% 
+% 解密
+% 
 decrypt(Words, Shift) -> encrypt(Words, -Shift).
 
-% todo
-slashes([]) -> [];
-slashes([ H | _ ]) when H =:= "~~" -> "\\";
-slashes([ H | T ]) -> [ H | slashes(T) ].
+% 
+% 转义
+% 
+slashes(Words) ->
+    re:replace(Words, "~n", "\n", [ global, { return, list } ]).
 
+% 
+% 位移
+% 
 shift(C, Shift) when ((C >= $A) and (C =< $Z)) ->
     Offset = Shift rem 26,
     if
-        Offset + C > $Z  ->
-             $A + (Offset + C) - $Z;
-        Offset + C < $A  ->
-             $Z + Offset + C;
-        true ->
-             C + Offset
+        Offset + C > $Z  -> $A + (Offset + C) - $Z;
+        Offset + C < $A  -> $Z + Offset + C;
+        true -> C + Offset
     end;
 shift(C, Shift) when ((C >= $a) and (C =< $z)) ->
     Offset = Shift rem 26,
 	if
-        Offset + C > $z  ->
-             $a + (Offset + C) - $z;
-        Offset + C < $a  ->
-             $z + Offset + C;
-        true ->
-             C + Offset
+        Offset + C > $z  -> $a + (Offset + C) - $z;
+        Offset + C < $a  -> $z + Offset + C;
+        true -> C + Offset
     end;
 shift(C, _) -> C.
 
+% 
+% 测试
+% 
 test() ->
-    Text = "hello world, this is Caesar cipher.\n",
+    Text = "hello world, this is Caesar cipher.~n",
     Cipher = encrypt(Text, 3),
     io:format(Cipher),
     io:format(decrypt(Cipher, 3)).
